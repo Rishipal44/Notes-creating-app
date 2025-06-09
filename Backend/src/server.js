8eb577401a56e223c8bfaf404390e68ab0cddfa7
Rishi Pal <rishikumarxyx@gmail.com>
2025-06-09 23:26:20 +0530

    update server js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 // middleware
 // if (process.env.NODE_ENV !== "production") {
