26b5da8107629bf7a29008396771a7e48830dc99
Rishi Pal <rishikumarxyx@gmail.com>
2025-06-09 23:22:32 +0530

    commit changed code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
+// import path from "path";
 
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -14,13 +14,19 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 // middleware
-if (process.env.NODE_ENV !== "production") {
-  app.use(
+// if (process.env.NODE_ENV !== "production") {
+//   app.use(
+//     cors({
+//       origin: "http://localhost:5173",
+//     })
+//   );
+// }
+
+app.use(
     cors({
       origin: "http://localhost:5173",
     })
   );
-}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
@@ -32,13 +38,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
