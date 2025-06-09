ec324bf408d78cd68c5aadf0011c778b566d06f9
Rishi Pal <rishikumarxyx@gmail.com>
2025-06-09 16:38:07 +0530

    commit code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./Config/db.js";
@@ -13,13 +14,27 @@ const app = express();
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+    app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, "..Frontend", "dist", "index.html"));
+})
+}
+
 
 
 connectDB().then(() => {
