d96c38af26ed79aaa3bcf41c98876ef16be3d922
Rishi Pal <rishikumarxyx@gmail.com>
2025-06-10 13:09:44 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import path from "path";
+import path from "path";
 
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,22 +11,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 // middleware
-// if (process.env.NODE_ENV !== "production") {
-//   app.use(
-//     cors({
-//       origin: "http://localhost:5173",
-//     })
-//   );
-// }
-
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
       origin: "http://localhost:5173",
     })
   );
+}
+
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
@@ -38,16 +33,16 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
+    console.log("Server started on PORT", PORT);
   });
 });
